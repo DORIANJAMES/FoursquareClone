ccfbe4d77381a9746344525bf9bbd0ff20bcb280
DORIANJAMES <dorianthelycan@gmail.com>
2022-10-04 13:56:02 +0300

    SignUp process completed just with userName and password variables only.

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -11,12 +11,21 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var usernameTextInput: UITextField!
+    @IBOutlet weak var passwordTextInput: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "Apple"
-        parseObject["calories"] = 100
+        
+        // PARSE üzerine bir FPObject kaydını gerçekleştiren fonksiyonlar.
+        /*let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
         
         parseObject.saveInBackground { success, error in
             if error != nil {
@@ -24,10 +33,60 @@ class ViewController: UIViewController {
             } else {
                 print("uploaded")
             }
-        }
+        }*/
+        
+        
+        // PARSE üzerindeki verilerin çekilme işlemini gerçekleştiren fonksiyonlar.
+        /*let query = PFQuery(className: "Fruits")
+        query.whereKey("calories", greaterThan: 100)
+        query.findObjectsInBackground { queryObject, error in
+            if error != nil {
+                
+            } else {
+                print(queryObject)
+            }
+        }*/
         
     }
 
-
+    @IBAction func loginButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+        if usernameTextInput.text != "" {
+            if passwordTextInput.text != "" {
+                let user = PFUser()
+                user.username = usernameTextInput.text
+                user.password = passwordTextInput.text
+                
+                user.signUpInBackground { (success,error) in
+                    if error != nil {
+                        self.makeAlert(alertTitle: "Sign Up Failed", alertMessage: error?.localizedDescription ?? "Error", alertStyle: UIAlertController.Style.alert, buttonTitle: "OK", buttonStyle: UIAlertAction.Style.default, isAnimated: true)
+                    } else {
+                        self.performSegue(withIdentifier: "logedin", sender: nil)
+                    }
+                }
+                
+            } else {
+                makeAlert(alertTitle: "Fields Empty", alertMessage: "You should chose a pasword to complete Sign Up process.", alertStyle: UIAlertController.Style.alert, buttonTitle: "OK", buttonStyle: UIAlertAction.Style.default, isAnimated: true)
+            }
+        } else {
+            makeAlert(alertTitle: "Fields Empty", alertMessage: "Username should be placed and be unique to continue on Sign Up process.", alertStyle: UIAlertController.Style.alert, buttonTitle: "OK", buttonStyle: UIAlertAction.Style.default, isAnimated: true)
+        }
+    }
+    
+    
+    
+    // Kullanıcıya gösterilecek Alert Fonksiyonu.
+    func makeAlert(alertTitle:String, alertMessage:String, alertStyle:UIAlertController.Style, buttonTitle:String, buttonStyle:UIAlertAction.Style, isAnimated:Bool) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: alertStyle)
+        let okButton = UIAlertAction(title: buttonTitle, style: buttonStyle)
+        alert.addAction(okButton)
+        self.present(alert, animated: isAnimated)
+    }
+    
+    func fetchFromParse() {
+        
+    }
 }
 
