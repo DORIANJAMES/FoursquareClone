add0fb98604020f9ad2ec4057a2adef1285b5722
Alihan AÇIKGÖZ <alihanacikgoz1@hotmail.com>
2022-10-11 14:44:17 +0300

    Fetching process done on DetialsVC.

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import MapKit
+import Parse
 
 class DetailsVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    var selectedPlaceName = ""
+    var selectedPlaceId = ""
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var placeNameLabel: UILabel!
@@ -19,9 +23,70 @@ class DetailsVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         mapView.delegate = self
+        fetchDataFromParse()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        selectedPlaceId.removeAll()
+        selectedPlaceName.removeAll()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        fetchDataFromParse()
+    }
+   
+    
+    
+    
+    func fetchDataFromParse () {
+        
+        let parse = PFQuery(className: "Places")
+        parse.whereKey("objectId", equalTo: selectedPlaceId)
+        parse.findObjectsInBackground { (parseObjects, error) in
+            if error != nil {
+                self.makeAlert(alertTitle: "Fetching Failed", alertMessage: "\(error?.localizedDescription ?? "Error")", alertStyle: UIAlertController.Style.alert, buttonTitle: "OK", buttonStyle: UIAlertAction.Style.default, handler: "failed")
+            } else {
+                for object in parseObjects! {
+                    if let name = object.object(forKey: "name") as? String {
+                        self.placeNameLabel.text = name
+                    }
+                    if let type = object.object(forKey: "type") as? String {
+                        self.placeTypeLabel.text = type
+                    }
+                    if let atmosphere = object.object(forKey: "atmosphere") as? String {
+                        self.placeAtmosphereLabel.text = atmosphere
+                    }
+                    if let image = object.object(forKey: "image") as? PFFileObject {
+                        image.getDataInBackground { fetchedImage, error in
+                            if error == nil {
+                                if fetchedImage != nil {
+                                    self.imageView.image = UIImage(data: fetchedImage!)
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    
+    
+    func makeAlert(alertTitle:String, alertMessage:String,alertStyle:UIAlertController.Style, buttonTitle:String, buttonStyle:UIAlertAction.Style, handler:String) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: alertStyle)
+        let okButton = UIAlertAction(title: buttonTitle, style: buttonStyle) { UIAlertAction in
+            switch (handler) {
+                case "done":
+                    break
+                case "failed":
+                    break
+                default:
+                    break
+            }
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
     
 
--- FoursquareClone/ListVC.swift
+++ FoursquareClone/ListVC.swift
@@ -10,36 +10,109 @@ import Parse
 
 class ListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let queryFromServer = PFQuery(className: "Places")
+   
+ 
+    
+    @IBOutlet weak var tableView: UITableView!
+    var placeNameArray = [String]()
+    var placeTypeArray = [String]()
+    var placeAtmosphereArray = [String]()
+    var placeImageArray = [Data]()
+    var placeObjectId = [String]()
+    var selectedPlaceName = ""
+    var selectedPlaceId = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getDataFromParse()
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 10
+        return placeNameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
+        var context = cell.defaultContentConfiguration()
+        context.text = placeNameArray[indexPath.row]
+        cell.contentConfiguration = context
         return cell
-    }
-    
-
-    @IBOutlet weak var tableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        
-       
-    }
-    
-    @objc func addNewPlace() {
-        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        selectedPlaceId = placeObjectId[indexPath.row]
+        selectedPlaceName = placeNameArray[indexPath.row]
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! DetailsVC
+        
+        destinationVC.selectedPlaceName = selectedPlaceName
+        destinationVC.selectedPlaceId = selectedPlaceId
+    }
+    
+    
+    
+    func getDataFromParse() {
+        let queryFromServer = PFQuery(className: "Places")
+        placeNameArray.removeAll()
+        placeTypeArray.removeAll()
+        placeAtmosphereArray.removeAll()
+        placeImageArray.removeAll()
+        placeObjectId.removeAll()
+        
+        queryFromServer.findObjectsInBackground { parseObjecs, error in
+            if error != nil {
+                self.makeAlert(alertTitle: "Fetching Failed", alertMessage: "\(error?.localizedDescription ?? "ERROR")", alertStyle: UIAlertController.Style.alert, buttonTitle: "OK", buttonStyle: UIAlertAction.Style.default, handler: "failed")
+            } else {
+                for object in parseObjecs! {
+                    if let name = object.object(forKey: "name") as? String {
+                        self.placeNameArray.append(name)
+                    }
+                    if let type = object.object(forKey: "type") as? String {
+                        self.placeTypeArray.append(type)
+                    }
+                    if let atmosphere = object.object(forKey: "atmosphere") as? String {
+                        self.placeAtmosphereArray.append(atmosphere)
+                    }
+                    if let image = object.object(forKey: "image") as? Data {
+                        self.placeImageArray.append(image)
+                    }
+                    if let id = object.objectId {
+                        self.placeObjectId.append(id)
+                    }
+                    
+                }
+                // Tablonun görünmesini istiyorsan bu fonksiyonu mutlaka çağırmak gerekli for'dan sonra.
+                self.tableView.reloadData()
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    func makeAlert (alertTitle:String,alertMessage:String,alertStyle:UIAlertController.Style, buttonTitle:String, buttonStyle:UIAlertAction.Style, handler:String) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: alertStyle)
+        let okButton = UIAlertAction(title: buttonTitle, style: buttonStyle) { UIAlertAction in
+            switch handler {
+                case "fetched":
+                    break
+                case "failed":
+                    break
+                default:
+                    break
+                    
+            }
+        }
     }
 }
